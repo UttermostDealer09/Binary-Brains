52e02f5085d593afee446f09e73372bf7d5b2240
Himesh Sharma <himeshsharma1011@gmail.com>
2026-04-06 17:03:39 +0530

    backkend: database setup

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ app.include_router(rank.router)
 @app.get("/")
 def root():
     return {"status": "ResumeRanker Running"}
+# utils/database.py
+
+from pymongo import MongoClient
+
+client = None
+db = None
+
+def connect_db():
+    global client, db
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["resumeranker"]
+
+def get_db():
+    return db
